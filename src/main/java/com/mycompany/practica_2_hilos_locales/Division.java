200884f3b66b13f3b661b8f2803387e1a95920e3
LoiueFragosoUwUr <bioshockgame422@gmail.com>
2023-03-10 15:46:24 -0600

    これは最初のプールです。

---
+++ src/main/java/com/mycompany/practica_2_hilos_locales/Division.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica_2_hilos_locales;
+import java.util.Scanner;
+/**
+ *
+ * @author almal
+ */
+public class Division extends Thread{
+    int x, y;
+    float ans;
+
+    public Division(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.ans =(float)x/y;
+        // floatValue of the Float Object
+    }
+
+    @Override
+    public void run() {
+        System.out.println("The result of the division is: "+ans);
+    }
+
+}
---
+++ src/main/java/com/mycompany/practica_2_hilos_locales/Multiplication.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica_2_hilos_locales;
+import java.util.Scanner;
+/**
+ *
+ * @author almal
+ */
+public class Multiplication extends Thread{
+    int x, y;
+    int ans = 0;
+
+    public Multiplication(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("The result of the multiplication is:"+(x*y));
+    }
+    
+}
